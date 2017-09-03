eb6a688b1501e5b0e71b5302fa7e036be3a28995
ryanmcstylin <ryanmcstylin@gmail.com>
2017-09-03 12:01:47 -0500

    Add files via upload

---
+++ FDIC/constants.py
@@ -0,0 +1,8 @@
+# Where will you save input and output files
+direc = 'C:/GitRepo/FDIC/PubData/'
+
+# Connection token to FFIEC webservice
+	#Create Account Here - https://cdr.ffiec.gov/public/PWS/
+ffiec_un = 'ryanmcstylin'
+ffiec_pw = 'CJ4hAT6n3yj1KBeb3fOk'
+
